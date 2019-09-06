52ea8e7e33b76b923ea01c576321beae5a966c0d
pascal Saalmüller <pasa02001@utb.vaxjo.se>
2019-09-06 09:43:51 +0200

    Har börgat med att data och användaren ska skriva in en kod. den visar sen ifall talaet va störe eller mindra

---
+++ Programering-Uppgift/Programering-Uppgift/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Programering_Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int RNumber = random.Next(0, 100);
+
+            int Snummer = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(RNumber);
+
+            if (Snummer > RNumber)
+                Console.WriteLine("Ditt tal är större");
+            if (Snummer < RNumber) 
+                Console.WriteLine("Ditt nummer är mindre");
+            else 
+                Console.WriteLine("Ditt nummer är lika stort");
+
+
+        }
+    }
+}
